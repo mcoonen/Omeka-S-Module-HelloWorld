dbb6a85e441752238bba892e6e7d0c7a1d7f6eef
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2026-03-19 13:22:14 +0100

    Added very rudimentary module config form

--- Module.php
+++ Module.php
@@ -1,12 +1,46 @@
 <?php
+
 namespace HelloWorld;
 
 use Omeka\Module\AbstractModule;
+use Laminas\Mvc\Controller\AbstractController;
+use Laminas\View\Renderer\PhpRenderer;
 
-class Module extends AbstractModule 
+class Module extends AbstractModule
 {
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
+
+    /**
+     * Get this module's configuration form.
+     *
+     * @param PhpRenderer $renderer
+     * @return string
+     */
+    public function getConfigForm(PhpRenderer $renderer)
+    {
+        return '
+        <label for="name">Enter a name:</label>
+        <input name="foo">
+        ';
+    }
+
+    /**
+     * Handle this module's configuration form.
+     *
+     * @param AbstractController $controller
+     * @return bool False if there was an error during handling
+     */
+
+    public function handleConfigForm(AbstractController $controller)
+    {
+        $request = $controller->getRequest();
+        $foo = $request->getPost('foo', '');
+        $controller->messenger()->addSuccess("Name entered: " . htmlspecialchars($foo));
+        return true;
+    }
+
 }
+
