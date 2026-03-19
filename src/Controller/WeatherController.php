f3eaf6f72413e177a32782d13a4f43c974fb6b5a
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2026-03-19 15:26:24 +0100

    Comments w.r.t. array syntax

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -10,12 +10,14 @@ class WeatherController extends AbstractActionController
 	public function indexAction()
 	{
 		$weather = array(
+            // Associative array (old syntax, but still supported in PHP 8)
 			'today' => 'Sunny, 18 °C',
 			'tomorrow' => 'Cloudy, 12 °C',
             'day after tomorrow' => 'Rainy, 11 °C',
         );
 
 		return new ViewModel([
+            // Also an associative array, but with the 'short array syntax' introduced in PHP 5.4
             'content' => $weather
         ]);
 
